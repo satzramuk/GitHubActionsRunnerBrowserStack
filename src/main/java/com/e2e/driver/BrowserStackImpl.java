7e026e07afdf7b652a97605f29a5e67fad3d9e7d
satzramuk <sathishndr92@gmail.com>
2022-08-14 17:47:07 +0530

    Github actions browserstack commit1

--- src/main/java/com/e2e/driver/BrowserStackImpl.java
+++ src/main/java/com/e2e/driver/BrowserStackImpl.java
@@ -18,57 +18,90 @@ public class BrowserStackImpl implements IDriver{
 		
 		//BrowserStack Jenkins Configuration Code Starts here
 		
-				String username = System.getenv("BROWSERSTACK_USERNAME");
-				String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-				String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
-				//String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
-				//String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-				String apppath = System.getenv("BROWSERSTACK_APP_ID");
-				caps.setCapability("browserstack.user",username );
-				caps.setCapability("browserstack.key", accessKey);
-				caps.setCapability("app", apppath);
-				caps.setCapability("device", "Google Pixel 3");
-				caps.setCapability("os_version", "9.0");
-				caps.setCapability("project", "First Framework Project");
-				caps.setCapability("build",buildName);
-				caps.setCapability("name", "first_FrameworkTest");
-				return new AndroidDriver<AndroidElement>(
-						new URL("http://hub.browserstack.com/wd/hub"), caps);
-
-				
-				
-				
+		/*
+		 * String username = System.getenv("BROWSERSTACK_USERNAME"); String accessKey =
+		 * System.getenv("BROWSERSTACK_ACCESS_KEY"); String buildName =
+		 * System.getenv("BROWSERSTACK_BUILD_NAME"); //String browserstackLocal =
+		 * System.getenv("BROWSERSTACK_LOCAL"); //String browserstackLocalIdentifier =
+		 * System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER"); String apppath =
+		 * System.getenv("BROWSERSTACK_APP_ID");
+		 * caps.setCapability("browserstack.user",username );
+		 * caps.setCapability("browserstack.key", accessKey); caps.setCapability("app",
+		 * apppath); caps.setCapability("device", "Google Pixel 3");
+		 * caps.setCapability("os_version", "9.0"); caps.setCapability("project",
+		 * "First Framework Project"); caps.setCapability("build",buildName);
+		 * caps.setCapability("name", "first_FrameworkTest"); return new
+		 * AndroidDriver<AndroidElement>( new URL("http://hub.browserstack.com/wd/hub"),
+		 * caps);
+		 * 
+		 * 
+		 * 
+		 */				
 				
 		//BrowserStack Jenkins Configuration Code Ends here
 
-//             Normal Execution Mode Starts here
-//		// Set your access credentials
-//		caps.setCapability("browserstack.user", "sathishkumar93");
-//		caps.setCapability("browserstack.key", "s14LDSWxcnzPK3LqoHzs");
-//
-//		// Set URL of the application under test
-//		caps.setCapability("app", "bs://b5c2a09780c3ea196ed8f50257472de53020a0f8");
-//
-//
-//		// Specify device and os_version for testing
-//		caps.setCapability("device", "Google Pixel 3");
-//		caps.setCapability("os_version", "9.0");
-//
-//		// Set other BrowserStack capabilities
-//		caps.setCapability("project", "First Framework Project");
-//		caps.setCapability("build", "Java Android");
-//		caps.setCapability("name", "first_FrameworkTest");
-//
-//
-//		// Initialize the remote Webdriver using BrowserStack remote URL
-//		// and desired capabilities defined above
-//
-//		return new AndroidDriver<AndroidElement>(
-//				new URL("http://hub.browserstack.com/wd/hub"), caps);
+         //    Normal Execution Mode Starts here
+		// Set your access credentials
+			/*
+			 * caps.setCapability("browserstack.user", "sathishkumar93");
+			 * caps.setCapability("browserstack.key", "s14LDSWxcnzPK3LqoHzs");
+			 * 
+			 * // Set URL of the application under test caps.setCapability("app",
+			 * "bs://b5c2a09780c3ea196ed8f50257472de53020a0f8");
+			 * 
+			 * 
+			 * // Specify device and os_version for testing caps.setCapability("device",
+			 * "Google Pixel 3"); caps.setCapability("os_version", "9.0");
+			 * 
+			 * // Set other BrowserStack capabilities caps.setCapability("project",
+			 * "First Framework Project"); caps.setCapability("build", "Java Android");
+			 * caps.setCapability("name", "first_FrameworkTest");
+			 * 
+			 * 
+			 * // Initialize the remote Webdriver using BrowserStack remote URL // and
+			 * desired capabilities defined above
+			 * 
+			 * return new AndroidDriver<AndroidElement>( new
+			 * URL("http://hub.browserstack.com/wd/hub"), caps);
+			 * 
+			 * 
+			 * 
+			 * 
+			 * }
+			 */	
+	 //    GitHub Actions Mode Starts here
+	// Set your access credentials
+	String username = System.getenv("BROWSERSTACK_USERNAME");
+	String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+	String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+	
+	caps.setCapability("browserstack.user",username );
+	caps.setCapability("browserstack.key",accessKey );
+
+	// Set URL of the application under test
+	caps.setCapability("app", "bs://425b95f0776e8a0d56d2e0596fae129b652f8d55");
+
+
+	// Specify device and os_version for testing
+	caps.setCapability("device", "Google Pixel 3");
+	caps.setCapability("os_version", "9.0");
+
+	// Set other BrowserStack capabilities
+	caps.setCapability("project", "First Framework Project");
+	caps.setCapability("build", "Java Android");
+	caps.setCapability("name", "first_FrameworkTest");
+
+
+	// Initialize the remote Webdriver using BrowserStack remote URL
+	// and desired capabilities defined above
+
+	return new AndroidDriver<AndroidElement>(
+			new URL("http://hub.browserstack.com/wd/hub"), caps);
 
 
 
-		
-	}
+	
+}
+
 
 }
