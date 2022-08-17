56ed9b5bfa80128480957088605d37a276506ecd
satzramuk <43157149+satzramuk@users.noreply.github.com>
2022-08-17 21:32:27 +0530

    Update BrowserStackImpl.java

--- src/main/java/com/e2e/driver/BrowserStackImpl.java
+++ src/main/java/com/e2e/driver/BrowserStackImpl.java
@@ -77,7 +77,7 @@ public class BrowserStackImpl implements IDriver{
 	 //    GitHub Actions Mode Starts here
 	// Set your access credentials
 	String username = System.getenv("BROWSERSTACK_USERNAME");
-	String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+	String accessKey = System.getenv("BROWSERSTACK_KEY");
 	String buildName = "TestFramework";
 	String appID = System.getenv("BROWSERSTACK_APP_ID");
 	
